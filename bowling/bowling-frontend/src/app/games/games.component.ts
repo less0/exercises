a046873411f360c3ba51b909ab1aab2737d7944a
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-08-26 12:13:45 +0200

    Bowling Kata: Implemented selecting games from the list

--- bowling/bowling-frontend/src/app/games/games.component.ts
+++ bowling/bowling-frontend/src/app/games/games.component.ts
@@ -3,18 +3,44 @@ import { Component, OnInit } from '@angular/core';
 import { GamesSummaries } from '../model/games-summaries';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { GameDetails } from '../model/game-details';
+import { GameSummary } from '../model/game-summary';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.less']
 })
-export class GamesComponent implements OnInit {
-  gamesSummaries$! : Observable<GamesSummaries>
+export class GamesComponent implements OnInit
+{
+  gamesSummaries$!: Observable<GamesSummaries>
+  selectedGame: GameDetails | undefined
 
-  public constructor(private httpClient : HttpClient) { }
+  public constructor(private httpClient: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.gamesSummaries$ = this.httpClient.get<GamesSummaries>(`${environment.apiUrl}/games`);
+  }
+
+  public selectGame(summary : GameSummary) : void
+  {
+    const detailsLink = summary.links!["details"];
+    if(detailsLink == undefined)
+    {
+      console.log("Details link is undefined");
+      
+      return;
+    }
+
+    console.log(`Getting ${detailsLink}`);
+    
+    this.httpClient.get<GameDetails>(`${environment.apiUrl}${detailsLink}`)
+                   .subscribe(gameDetails => this.selectedGame = gameDetails);
+  }
+
+  public unselectGame() : void
+  {
+    this.selectedGame = undefined;
   }
 }
