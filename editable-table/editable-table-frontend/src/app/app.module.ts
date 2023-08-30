70e790c9965a34aed0ddf9b527456b029261f24f
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-08-30 21:33:27 +0200

    Editable Table: Build up basic structure of the application

--- editable-table/editable-table-frontend/src/app/app.module.ts
+++ editable-table/editable-table-frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatCardModule } from '@angular/material/card'
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditableTableComponent } from './editable-table/editable-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditableTableComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ editable-table/editable-table-frontend/src/app/editable-table/editable-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editable-table',
+  templateUrl: './editable-table.component.html',
+  styleUrls: ['./editable-table.component.less']
+})
+export class EditableTableComponent {
+
+}
