b658babc5a5cdbf4625b12134246321f3e63cf89
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-09-02 21:24:39 +0200

    RabbitMQ Playground: Implemented the "Hello, world" example

--- rabbitmq-playground/consumer-worker/RabbitMqConsumer.cs
+++ rabbitmq-playground/consumer-worker/RabbitMqConsumer.cs
@@ -1,26 +1,89 @@
+using System.Text;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
 
 namespace consumer_worker;
 
 internal class RabbitMqConsumer : IHostedService
 {
     private readonly ILogger<RabbitMqConsumer> _logger;
+    private IConnection? _connection;
+    private IModel? _channel;
+    private EventingBasicConsumer? _consumer;
 
     public RabbitMqConsumer(ILogger<RabbitMqConsumer> logger)
     {
         _logger = logger;
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    private bool IsConnected => _connection?.IsOpen ?? false;
+
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
+        await ConnectWithRetryAsync(cancellationToken);
         _logger.LogInformation("StartAsync was called.");
-        return Task.CompletedTask;
+        return;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _connection?.Dispose();
+        _channel?.Dispose();
+
         _logger.LogInformation("StopAsync was called.");
         return Task.CompletedTask;
+    }
+
+    private async Task ConnectWithRetryAsync(CancellationToken cancellationToken)
+    {
+        TimeSpan timeout = TimeSpan.FromMinutes(5);
+        DateTime startedAt = DateTime.Now;
+
+        while (!cancellationToken.IsCancellationRequested && !IsConnected)
+        {
+            try
+            {
+                Connect();
+            }
+            catch (BrokerUnreachableException)
+            {
+                if (DateTime.Now - startedAt > timeout)
+                {
+                    _logger.LogCritical("Connection timeout exceeded.");
+                    throw new TimeoutException();
+                }
+
+                _logger.LogWarning("Connection failed. Retrying in 5 s.");
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+            }
+        }
+    }
+
+    private void Connect()
+    {
+        ConnectionFactory factory = new() { HostName = "rabbitmq" };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: "hello",
+             durable: false,
+             exclusive: false,
+             autoDelete: false,
+             arguments: null);
+
+        _consumer = new EventingBasicConsumer(_channel);
+        _consumer.Received += OnMessageReceived;
+        _channel.BasicConsume(queue: "hello",
+                             autoAck: true,
+                             consumer: _consumer);
+    }
+
+    private void OnMessageReceived(object? sender, BasicDeliverEventArgs e)
+    {
+        var body = e.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        _logger.LogInformation("Received message: {message}", message);
     }
 }
--- rabbitmq-playground/producer-worker/RabbitMqProducer.cs
+++ rabbitmq-playground/producer-worker/RabbitMqProducer.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 using Timer = System.Timers.Timer;
 
 namespace producer_worker;
@@ -7,14 +10,75 @@ namespace producer_worker;
 internal class RabbitMqProducer : IHostedService
 {
     private readonly ILogger<RabbitMqProducer> _logger;
-    private Timer _timer;
+    private Timer? _timer;
+    private IConnection? _connection;
+    private IModel? _channel;
 
     public RabbitMqProducer(ILogger<RabbitMqProducer> logger)
     {
         _logger = logger;
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    private bool IsConnected => _connection?.IsOpen ?? false;
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await ConnectWithRetryAsync(cancellationToken);
+        StartTimer();
+
+        _logger.LogInformation("StartAsync was called.");
+        return;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _timer?.Dispose();
+        _connection?.Dispose();
+        _channel?.Dispose();
+        _logger.LogInformation("StopAsync was called");
+        return Task.CompletedTask;
+    }
+
+    private async Task ConnectWithRetryAsync(CancellationToken cancellationToken)
+    {
+        TimeSpan timeout = TimeSpan.FromMinutes(5);
+        DateTime startedAt = DateTime.Now;
+
+        while (!cancellationToken.IsCancellationRequested
+            && !IsConnected)
+        {
+            try
+            {
+                Connect();
+            }
+            catch (BrokerUnreachableException)
+            {
+                if (DateTime.Now - startedAt > timeout)
+                {
+                    _logger.LogCritical("Connection timeout exceeded.");
+                    throw new TimeoutException();
+                }
+
+                _logger.LogWarning("Connection failed. Retrying in 5 s.");
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+            }
+        }
+    }
+
+    private void Connect()
+    {
+        var factory = new ConnectionFactory { HostName = "rabbitmq" };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+        _channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+    }
+
+    private void StartTimer()
     {
         _timer = new()
         {
@@ -24,20 +88,32 @@ internal class RabbitMqProducer : IHostedService
         };
 
         _timer.Elapsed += OnTimerElapsed;
-
-        _logger.LogInformation("StartAsync was called.");
-        return Task.CompletedTask;
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        _timer.Dispose();
-        _logger.LogInformation("StopAsync was called");
-        return Task.CompletedTask;
     }
 
     private void OnTimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
     {
         _logger.LogInformation("Timer elapsed.");
+        if (!IsConnected)
+        {
+            _logger.LogInformation("Not connected.");
+            return;
+        }
+
+        SendMessage();
+    }
+
+    private void SendMessage()
+    {
+        _logger.LogInformation("Sending message...");
+
+        const string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        _channel.BasicPublish(exchange: string.Empty,
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+
+        _logger.LogInformation("Sent message.");
     }
 }
