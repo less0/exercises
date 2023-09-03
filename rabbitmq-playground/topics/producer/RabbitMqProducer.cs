89324c412f671df5e869aae2e0d49e31cb6a0e58
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-09-03 15:00:23 +0200

    RabbitMQ Playground: Implemented timer for topic producer

--- rabbitmq-playground/topics/producer/RabbitMqProducer.cs
+++ rabbitmq-playground/topics/producer/RabbitMqProducer.cs
@@ -1,19 +1,31 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Timer = System.Timers.Timer;
+
 namespace producer;
 
 internal class RabbitMqProducer : IHostedService
 {
     private readonly ILogger<RabbitMqProducer> _logger;
+    private readonly Timer _timer;
 
     public RabbitMqProducer(ILogger<RabbitMqProducer> logger)
     {
         _logger = logger;
+        _timer = new()
+        {
+            Enabled = false,
+            AutoReset = true,
+            Interval = 2000.0
+        };
+        _timer.Elapsed += OnTimerElapsed;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        _timer.Enabled = true;
+
         _logger.LogInformation("Started service.");
         return Task.CompletedTask;
     }
@@ -22,5 +34,10 @@ internal class RabbitMqProducer : IHostedService
     {
         _logger.LogInformation("Stopped service");
         return Task.CompletedTask;
+    }
+
+    private void OnTimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        _logger.LogInformation("Timer elapsed.");
     }
 }
