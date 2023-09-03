37a6712662eebabc78ca12ded75a015a01912d28
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-09-03 15:40:18 +0200

    RabbitMQ Playground: Implemented topic producer

--- rabbitmq-playground/topics/producer/RabbitMqProducer.cs
+++ rabbitmq-playground/topics/producer/RabbitMqProducer.cs
@@ -1,6 +1,9 @@
+using System.Text;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 using Timer = System.Timers.Timer;
 
 namespace producer;
@@ -8,9 +11,13 @@ namespace producer;
 internal class RabbitMqProducer : IHostedService
 {
     private readonly ILogger<RabbitMqProducer> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly string _topic;
     private readonly Timer _timer;
+    private IConnection? _connection;
+    private IModel? _channel;
 
-    public RabbitMqProducer(ILogger<RabbitMqProducer> logger)
+    public RabbitMqProducer(ILogger<RabbitMqProducer> logger, IConfiguration configuration)
     {
         _logger = logger;
         _timer = new()
@@ -20,18 +27,26 @@ internal class RabbitMqProducer : IHostedService
             Interval = 2000.0
         };
         _timer.Elapsed += OnTimerElapsed;
+        _configuration = configuration;
+        _topic = _configuration["Topic"]!;
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    public bool IsConnected => _connection?.IsOpen ?? false;
+
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
+        await ConnectWithRetryAsync(cancellationToken);
         _timer.Enabled = true;
 
         _logger.LogInformation("Started service.");
-        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _timer.Dispose();
+        _connection?.Dispose();
+        _channel?.Dispose();
+
         _logger.LogInformation("Stopped service");
         return Task.CompletedTask;
     }
@@ -39,5 +54,67 @@ internal class RabbitMqProducer : IHostedService
     private void OnTimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
     {
         _logger.LogInformation("Timer elapsed.");
+
+        if(!IsConnected)
+        {
+            _logger.LogWarning("Not connected.");
+            return;
+        }
+
+        _logger.LogInformation("Sending message");
+        var (message, severity) = Random.Shared.NextDouble() switch
+        {
+            <.1 => ("Error message", "error"),
+            <.3 => ("Warning message", "warning"), 
+            _ => ("Info message", "info")
+        };
+
+        _channel.BasicPublish("logs",
+            $"{_topic}.{severity}",
+            null,
+            Encoding.UTF8.GetBytes(message));
+    }
+
+    private async Task ConnectWithRetryAsync(CancellationToken cancellationToken)
+    {
+        var timeout = TimeSpan.FromMinutes(5);
+        var startedAt = DateTime.Now;
+
+        _logger.LogInformation("Connecting...");
+
+        while(!cancellationToken.IsCancellationRequested
+            && !IsConnected)
+        {
+            try
+            {
+                Connect();
+                _logger.LogInformation("Connected.");
+            }
+            catch (BrokerUnreachableException)
+            {
+                if(DateTime.Now - startedAt > timeout)
+                {
+                    _logger.LogCritical("Connection timeout exceeded.");
+                    throw new TimeoutException();
+                }
+                
+                _logger.LogWarning("Connection failed. Retrying in 5 s.");
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+            }
+        }
+            
+    }
+
+    private void Connect()
+    {
+        ConnectionFactory connectionFactory = new()
+        {
+            HostName = "rabbitmq"
+        };
+
+        _connection = connectionFactory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.ExchangeDeclare("logs",
+            ExchangeType.Topic);
     }
 }
