bb7535eba2be513f12507ac15e5b99dedb8a494f
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-09-13 01:37:08 +0200

    RabbitMQ Playground: Created scaffold for RPC

---
+++ rabbitmq-playground/rpc/producer/Program.cs
@@ -0,0 +1,9 @@
+﻿using Kertscher.RabbitMq.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var hostBuilder = Host.CreateApplicationBuilder();
+hostBuilder.Services.AddHostedService<RabbitMqWorker>();
+
+var host = hostBuilder.Build();
+host.Run();
