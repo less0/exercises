f110df79c4078803767cd1a2feaa52c492cea7f3
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-09-13 02:05:23 +0200

    RabbitMQ Playground: Started implementing RPC producer

--- rabbitmq-playground/rpc/producer/Program.cs
+++ rabbitmq-playground/rpc/producer/Program.cs
@@ -1,9 +1,10 @@
 ﻿using Kertscher.RabbitMq.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using producer;
 
 var hostBuilder = Host.CreateApplicationBuilder();
-hostBuilder.Services.AddHostedService<RabbitMqWorker>();
+hostBuilder.Services.AddHostedService<RpcProducer>();
 
 var host = hostBuilder.Build();
 host.Run();
---
+++ rabbitmq-playground/rpc/producer/RpcProducer.cs
@@ -0,0 +1,44 @@
+﻿using System.Timers;
+using Kertscher.RabbitMq.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using Timer = System.Timers.Timer;
+
+namespace producer;
+
+public class RpcProducer : RabbitMqWorker
+{
+    private readonly ILogger<RpcProducer> _logger;
+    private readonly Timer _timer;
+
+    public RpcProducer(ILogger<RpcProducer> logger, IConfiguration configuration) 
+        : base(logger, configuration)
+    {
+        _logger = logger;
+        _timer = new()
+        {
+            Enabled = false,
+            AutoReset = true,
+            Interval = 5000
+        };
+        _timer.Elapsed += TimerOnElapsed;
+    }
+
+    protected override async Task OnConnected(IConnection connection, bool reconnected)
+    {
+        await base.OnConnected(connection, reconnected);
+        _timer.Start();
+    }
+
+    private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+    {
+        _logger.LogInformation("Timer elapsed");
+
+        if (!IsConnected)
+        {
+            _logger.LogError("Not connected.");
+            return;
+        }
+    }
+}
